ff6fbf63c34eb15ec31f5cc2a41de8da6d872a77
Daniel201904 <danivar763@gmail.com>
2024-12-06 15:18:56 -0500

    Se hace commit de database service y la injeccion de dependencias

--- obj/Debug/net7.0/E-Commerce.AssemblyInfo.cs
+++ obj/Debug/net7.0/E-Commerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cafd1903bf5a7fbeca7f4e82e4276582b75424b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7d836c6c92051a1bb9c73e4072783e9fad3b6c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
