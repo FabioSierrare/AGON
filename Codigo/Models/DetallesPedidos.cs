1f0b4e2663c171425bd22fb9268bf665124e94cb
Daniel.P <121536887+danielpa512@users.noreply.github.com>
2024-12-13 14:03:10 -0500

    Add files via upload

---
+++ Codigo/Models/DetallesPedidos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Models
+{
+    public class DetallesPedidos
+    {
+        public int Id { get; set; }
+        public int PedidoId { get; set; }
+
+        public int ProductoId { get; set; }
+
+        public int Cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
+    }
+}
