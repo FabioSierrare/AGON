1c14f0cf11d77b9363fb35a7a6f14f54caf81832
Daniel.P <121536887+danielpa512@users.noreply.github.com>
2024-12-13 12:53:24 -0500

    Add files via upload

---
+++ Codigo/DepencyInjectionService.cs
@@ -0,0 +1,46 @@
+﻿using E_Commerce.Repositories.Interfaces;
+using E_Commerce.Repositories;
+using E_Commerce.Repositories;
+using E_Commerce.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using MicroServiceCRUD.Repositories;
+
+namespace MicroServiceCRUD
+{
+    public static class DependencyInjectionService
+    {
+        public static IServiceCollection AddExternal(this IServiceCollection services, IConfiguration _configuration)
+        {
+            // Obtener la cadena de conexión desde la configuración
+            string connectionString = _configuration["ConnectionStrings:SQLConnectionStrings"];
+
+            // Registrar el DbContext para la base de datos
+            services.AddDbContext<DatabaseService>(options => options.UseSqlServer(connectionString));
+
+            // Registrar interfaces sin el sufijo Repository
+            services.AddScoped<ICategoria, CategoriaRepository>();
+            services.AddScoped<IComentarios, ComentariosRepository>();
+            services.AddScoped<ICupones, CuponesRepository>();
+            services.AddScoped<IDetallesPedidos, DetallesPedidosRepository>();
+            services.AddScoped<IEmpresasEnvio, EmpresasEnvioRepository>();
+            services.AddScoped<IEnvios, EnviosRepository>();
+            services.AddScoped<IImagenProducto, ImagenProductoRepository>();
+            services.AddScoped<IInventarios, InventariosRepository>();
+            services.AddScoped<ILogsSistema, LogsSistemaRepository>();
+            services.AddScoped<INotificaciones, NotificacionesRepository>();
+            services.AddScoped<IPedidos, PedidosRepository>();
+            services.AddScoped<IPermiso, PermisoRepository>();
+            services.AddScoped<IProductos, ProductosRepository>();
+            services.AddScoped<IPromociones, PromocionesRepository>();
+            services.AddScoped<IReporteAcciones, ReporteAccionesRepository>();
+            services.AddScoped<IRespuestasFAQ, RespuestasFAQRepository>();
+            services.AddScoped<IRolesPermisos, RolesPermisosRepository>();
+            services.AddScoped<ITicketsSoporte, TicketsSoporteRepository>();
+            services.AddScoped<ITrackingEnvio, TrackingEnvioRepository>();
+            services.AddScoped<IUsuarios, UsuariosRepository>();
+            services.AddScoped<IValoraciones, ValoracionesRepository>();
+
+            return services;
+        }
+    }
+}
