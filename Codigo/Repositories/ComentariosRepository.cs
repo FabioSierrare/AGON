bcfcf627255f5b5e95184298d8d838084249c3ef
Daniel.P <121536887+danielpa512@users.noreply.github.com>
2024-12-13 13:55:55 -0500

    Add files via upload

---
+++ Codigo/Repositories/ComentariosRepository.cs
@@ -0,0 +1,47 @@
+﻿using E_Commerce.Models;
+using E_Commerce.Context;
+using E_Commerce.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce.Repositories
+{
+    public class ComentariosRepository : IComentarios
+    {
+        public readonly E_commerceContext context; // Usar 'context' en lugar de '_context'
+
+        public ComentariosRepository(E_commerceContext context)
+        {
+            this.context = context; // Se inyecta el contexto en el constructor
+        }
+
+        public async Task<List<Comentarios>> GetComentarios()
+        {
+            var data = await context.Comentarios.ToListAsync(); // Usar 'context'
+            return data;
+        }
+
+        public async Task<bool> PostComentarios(Comentarios comentarios)
+        {
+            await context.Comentarios.AddAsync(comentarios); // Usar 'context'
+            await context.SaveChangesAsync(); // Corregir 'SaveAsync' por 'SaveChangesAsync'
+            return true;
+        }
+
+        public async Task<bool> PutComentarios(Comentarios comentarios )
+        {
+            context.Comentarios.Update(comentarios);
+            await context.SaveAsync();
+            return true;
+        }
+
+        public async Task<bool> DeleteComentarios(int id)
+        {
+            var comentario = await context.Comentarios.FindAsync(id); // Usar 'context' en lugar de '_context'
+            if (comentario == null) return false; // Si no existe, devolver 'false'
+
+            context.Comentarios.Remove(comentario); // Usar 'context'
+            await context.SaveChangesAsync(); // Corregir 'SaveAsync' por 'SaveChangesAsync'
+            return true;
+        }
+    }
+}
---
+++ Codigo/Repositories/RespuestasFAQRepository.cs
@@ -0,0 +1,45 @@
+﻿using E_Commerce.Models;
+using E_Commerce.Context;
+using E_Commerce.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce.Repositories
+{
+    public class RespuestasFAQRepository : IRespuestasFAQ
+    {
+        private readonly E_commerceContext context;
+
+        public RespuestasFAQRepository(E_commerceContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<RespuestasFAQ>> GetRespuestasFAQ()
+        {
+            var data = await context.RespuestasFAQ.ToListAsync();
+            return data;
+        }
+
+        public async Task<bool> PostRespuestaFAQ(RespuestasFAQ respuestasFAQ)
+        {
+            await context.RespuestasFAQ.AddAsync(respuestasFAQ);
+            await context.SaveAsync();
+            return true;
+        }
+        public async Task<bool> PutRespuestasFAQ(RespuestasFAQ respuestasFAQ)
+        {
+            context.RespuestasFAQ.Update(respuestasFAQ);
+            await context.SaveAsync();
+            return true;
+        }
+        public async Task<bool> DeleteRespuestasFAQ(int id)
+        {
+            var notificacion = await context.RespuestasFAQ.FindAsync(id);
+            if (notificacion == null) return false;
+
+            context.RespuestasFAQ.Remove(notificacion);
+            await context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
