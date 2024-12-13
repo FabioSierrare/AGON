cb04dc1c75507782bfad2e1b23046a0b0ad51435
Daniel.P <121536887+danielpa512@users.noreply.github.com>
2024-12-13 14:07:18 -0500

    Delete Repositories directory

--- Repositories/Interfaces/ICategoria.cs
+++
@@ -1,9 +0,0 @@
-﻿using E_Commerce.Models;
-
-public interface ICategoria
-{
-    Task<List<Categoria>> GetCategoria();
-    Task<bool> PostCategoria(Categoria categoria);
-    Task<bool> PutCategoria(Categoria categoria);
-    Task<bool> DeleteCategoria(int id);  // Este método debe estar aquí
-}
