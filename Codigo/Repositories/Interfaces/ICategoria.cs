239b904b728e1ff2d95b859ae403a8b54ed94c4e
Daniel.P <121536887+danielpa512@users.noreply.github.com>
2024-12-13 13:56:46 -0500

    Add files via upload

---
+++ Codigo/Repositories/Interfaces/ICategoria.cs
@@ -0,0 +1,9 @@
+﻿using E_Commerce.Models;
+
+public interface ICategoria
+{
+    Task<List<Categoria>> GetCategoria();
+    Task<bool> PostCategoria(Categoria categoria);
+    Task<bool> PutCategoria(Categoria categoria);
+    Task<bool> DeleteCategoria(int id);  // Este método debe estar aquí
+}
